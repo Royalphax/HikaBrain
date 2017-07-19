7f32e8f24f5eca05e609589e8a6309d1ec00d1cd
Roytreo28 <therenceforot@orange.fr>
2017-07-19 13:04:17 +0200

    Update main class

--- HikaBrain/src/fr/roytreo/hikabrain/core/HikaBrainPlugin.java
+++ HikaBrain/src/fr/roytreo/hikabrain/core/HikaBrainPlugin.java
@@ -9,30 +9,34 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.roytreo.hikabrain.core.event.EventListener;
+import fr.roytreo.hikabrain.core.event.arena.ArenaPlayerRespawn;
+import fr.roytreo.hikabrain.core.event.arena.PlayerClickArenaSign;
+import fr.roytreo.hikabrain.core.event.arena.PlayerJoinArena;
+import fr.roytreo.hikabrain.core.event.arena.UpdateSign;
+import fr.roytreo.hikabrain.core.event.block.BlockBreak;
 import fr.roytreo.hikabrain.core.event.player.PlayerDeath;
 import fr.roytreo.hikabrain.core.event.player.PlayerInteract;
+import fr.roytreo.hikabrain.core.event.player.PlayerQuit;
 import fr.roytreo.hikabrain.core.event.sign.SignChange;
+import fr.roytreo.hikabrain.core.handler.Exception;
 import fr.roytreo.hikabrain.core.handler.MinecraftVersion;
 import fr.roytreo.hikabrain.core.manager.VersionManager;
-import fr.roytreo.hikabrain.core.util.Utils;
 import net.md_5.bungee.api.ChatColor;
 
 public class HikaBrainPlugin extends JavaPlugin {
 
 	/**
 	 * TODO:
-	 * - Système panneau cliquable (fait plus ou moins)
 	 * - faire que lorsque un joueur va sur la laine adverse, point pour l'equipe.
 	 * - faire que le spawn est automatiquement 4 bloques au dessus de la laine ?
 	 * - Faire BeginCountdown & Game tasks
 	 * - Faire systeme de setup de la map
-	 * - Faire differentes game states
 	 * - Empecher de move des choses de l'inventaire lorsque on est en waiting room
 	 */
 	
 	private static HikaBrainPlugin instance;
 	public static final String PACKAGE = "fr.roytreo.hikabrain";
-	public static final String PREFIX = ChatColor.GRAY + "[" + ChatColor.GOLD + "HikaBrain" + ChatColor.GRAY + "] " + ChatColor.RESET;
+	public static String PREFIX = ChatColor.GRAY + "[" + ChatColor.GOLD + "HikaBrain" + ChatColor.GRAY + "] " + ChatColor.RESET;
 
 	public VersionManager versionManager;
 
@@ -51,10 +55,12 @@ public class HikaBrainPlugin extends JavaPlugin {
 		try {
 			(this.versionManager = new VersionManager(MinecraftVersion.getVersion())).load();
 		} catch (ReflectiveOperationException e1) {
-			Utils.registerException(e1, true);
+			new Exception(e1).register(HikaBrainPlugin.getInstance(), true);
 			return;
 		}
-		this.register(PlayerDeath.class, PlayerInteract.class, SignChange.class);
+		this.register(PlayerDeath.class, PlayerInteract.class, SignChange.class,
+				ArenaPlayerRespawn.class, PlayerClickArenaSign.class, PlayerJoinArena.class,
+				UpdateSign.class, BlockBreak.class, PlayerQuit.class);
 	}
 
 	@Override
@@ -77,11 +83,11 @@ public class HikaBrainPlugin extends JavaPlugin {
 				Bukkit.getPluginManager().registerEvents((Listener) constructor.newInstance(this), (Plugin) this);
 			}
 		} catch (Throwable ex) {
-			Utils.registerException(ex, true);
+			new Exception(ex).register(HikaBrainPlugin.getInstance(), true);
 			try {
 				throw ex;
 			} catch (Throwable e) {
-				Utils.registerException(e, true);
+				new Exception(e).register(HikaBrainPlugin.getInstance(), true);
 			}
 		}
 	}
@@ -90,9 +96,13 @@ public class HikaBrainPlugin extends JavaPlugin {
 		try {
 			Class.forName("org.bukkit.entity.Player$Spigot");
 			return true;
-		} catch (Exception e) {
+		} catch (java.lang.Exception e) {
 			return false;
 		}
+	}
+	
+	public static void updatePrefix(String newPrefix) {
+		PREFIX = newPrefix;
 	}
 
 }
