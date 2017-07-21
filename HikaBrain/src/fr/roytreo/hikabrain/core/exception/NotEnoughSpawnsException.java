c95343eaadb559ebdc769458cd44aa6d24aa0bb2
Roytreo28 <therenceforot@orange.fr>
2017-07-21 17:09:49 +0200

    Add Exception

---
+++ HikaBrain/src/fr/roytreo/hikabrain/core/exception/NotEnoughSpawnsException.java
@@ -0,0 +1,10 @@
+package fr.roytreo.hikabrain.core.exception;
+
+public class NotEnoughSpawnsException extends Exception {
+	
+	private static final long serialVersionUID = 5011974906036579308L;
+
+	public NotEnoughSpawnsException(String message) {
+		super(message);
+	}
+}
